c4848e9726b0b2d325b40c4a441c9f7763e5ec60
giuperi <giuseppe.peri@capgemini.com>
2021-03-18 16:31:12 +0100

    module 3

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Album } from 'app/album';
+import { ProductService } from 'app/product.service';
 
 @Component({
   selector: 'app-product-description',
@@ -7,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDescriptionComponent implements OnInit {
 
-  constructor() { }
+  albumInfo: Album;
+
+  constructor(private _productService: ProductService) { }
 
   ngOnInit() {
+    this._productService.getAlbum(1).subscribe(
+      response => this.albumInfo = response
+    )
   }
 
 }
