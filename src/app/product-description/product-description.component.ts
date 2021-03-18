7f623e865d898cab39305d64ce0b12e6495fa994
giuperi <giuseppe.peri@capgemini.com>
2021-03-18 16:33:10 +0100

    module 3 fix

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Album } from 'app/album';
-import { ProductService } from 'app/product.service';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-product-description',
